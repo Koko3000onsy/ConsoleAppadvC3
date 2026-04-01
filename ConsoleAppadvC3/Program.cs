e8d8a99943f715a9a82dbdc3b7a8bc530c869145
Koko3000onsy <onsykoko79@gmail.com>
2026-04-01 23:01:31 +0200

    Q1

--- ConsoleAppadvC3/Program.cs
+++ ConsoleAppadvC3/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           
+            //List<int> grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+            //foreach (var grade in grades)
+            //{
+            //    Console.WriteLine($"Grade: {grade}");
+
+            //}
+            //Console.WriteLine($"count: {grades.Count}");
+            //Console.WriteLine($"first grade: {grades[0]}");
+            //Console.WriteLine($"last grade: {grades[grades.Count - 1]}");
+            //grades.Sort();
+            //foreach (var grade in grades)
+            //{
+            //    Console.WriteLine($"Grade: {grade}");
+            //}
+            //var topGrades = grades.FirstOrDefault(g => g >= 90);
+            //Console.WriteLine($"Top Grades:{topGrades}");
+            //var below70Grades = grades.Where(g => g < 70).ToList();
+            //Console.WriteLine("Grades below 70:");
+            //foreach (var grade in below70Grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            //grades.RemoveAll(g => g < 75);
+            //Console.WriteLine("Grades after removing below 75:");
+            //foreach (var grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            //bool hasPerfectScore = grades.Any(g => g == 100);
+            //Console.WriteLine($"Has perfect score: {hasPerfectScore}");
+            //List<string> gradeinstring = grades.Select(g =>  $"Grade: {g}").ToList();
+            //Console.WriteLine("Grades as strings:");
+            //foreach (var grade in gradeinstring)
+            //{
+            //    Console.WriteLine(grade);
+
+            //}
         }
     }
 }
